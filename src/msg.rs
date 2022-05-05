874aee6eba97b461df9b1280c9445b3c1df30e1e
ask03 <aykim043@gmail.com>
2022-05-04 17:39:54 -0700

    cosmwasm assignment/deleted uneeded msgs

--- src/msg.rs
+++ src/msg.rs
@@ -21,7 +21,3 @@ pub enum QueryMsg {
 }
 
 // We define a custom struct for each query response
-#[derive(Serialize, Deserialize, Clone, Debug, PartialEq, JsonSchema)]
-pub struct CountResponse {
-    pub count: i32,
-}
